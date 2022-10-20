a42333c0010a41e36c40ace762bcadab287e06d1
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-20 17:22:05 -0500

    stopping point 43:06

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+// This is react without using JSX line 3 to 6
+// const Star = () => React.createElement('div', null, 'This is a little star');
+// const App = () => {
+//   return Star();
+// }
 function App() {
   return (
-    <div className="App">
+    <div className = "App">
       Start here.
     </div>
   );
