045178cf4170bfc18fe17582bd192f41bf1278e0
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-21 20:10:47 -0500

    importing files for movie app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+
+// Styles
+import { GlobalStyle } from './GlobalStyle';
+
 // This is react without using JSX line 3 to 6
 // const Star = () => React.createElement('div', null, 'This is a little star');
 // const App = () => {
@@ -8,6 +12,7 @@ function App() {
   return (
     <div className = "App">
       Start here.
+      {/* <GlobalStyle /> */}
     </div>
   );
 }
