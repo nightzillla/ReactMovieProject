04aea405601f43a196ef689b26157f73a3d69a06
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-21 22:36:10 -0500

    learning useState

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+// Config
+import { POSTER-SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+// Components
+
+// Hook
+
+// Image
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+    const [state, setState] = useState();
+
+    return <div>Home Page</div>
+}
