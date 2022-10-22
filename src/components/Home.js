d51d475ee9b5d1586dc7bab346611232bf4de54d
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-22 01:58:01 -0500

    working on fixing react 18 issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
+import HeroImage from './HeroImage';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -13,7 +14,18 @@ const Home = () => {
     const { state, loading , error } = useHomeFetch();
     console.log(state)
 
-    return <div>Home Page</div>
-}
+    return(
+        <>
+        {state.results[0] ?
+        <HeroImage 
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            title={state.results[0].orginal_title}
+            text={state.results[0].overview}
+        /> 
+        : null
+        }
+        </>
+    )
+};
 
 export default Home; 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+// import React from 'react';
+// // import ReactDOM from 'react-dom';
+// // new reactDOM
+// import ReactDOM from "react-dom/client"; 
+// import App from './App';
+
+// // const root = ReactDOM.createRoot(document.getElementById("root"))
+// // root.render(<App />);
+
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('app');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
