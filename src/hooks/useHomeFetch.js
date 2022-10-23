fb58249199842007826333d9dfa7b8a5586ea4f8
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-23 16:52:06 -0500

    got loading button functions to load new movies!

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -17,6 +17,7 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
     console.log(searchTerm);
 
     const fetchMovies = async (page, searchTerm = "") => {
@@ -45,5 +46,14 @@ export const useHomeFetch = () => {
      fetchMovies(1, searchTerm);
     }, [searchTerm]);
 
-    return { state, loading, error, searchTerm, setSearchTerm };
+    // This is what will load new pages
+    // Load more
+    useEffect(() => {
+        if(!isLoadingMore) return;
+
+        fetchMovies(state.page + 1, searchTerm)
+        setIsLoadingMore(false);
+    },[isLoadingMore, searchTerm, state.page])
+
+    return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 }
