c247c0645a50a70e57dd3c669860b3e821fcaead
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-29 14:53:14 -0500

    working on index.js

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,11 +1,4 @@
 import react from 'react';
 
-import { Wrapper } from './Button.style';
-
-const Button = ({ text, callback }) => (
-    <Wrapper type="button" onClick={callback}>
-        {text}
-    </Wrapper>
-);
 
 export default Button;
